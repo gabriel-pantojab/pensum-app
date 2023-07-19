6ac7099527237ca093b11ed8607db6890b9826d3
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-07-19 09:46:58 -0300

    [FIX] content pensum

--- src/components/pensum/Pensum.ios.jsx
+++ src/components/pensum/Pensum.ios.jsx
@@ -12,6 +12,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
+    flex: 1,
     padding: 10,
     gap: 20,
     backgroundColor: theme.colors.white,
--- src/components/pensum/Pensum.jsx
+++ src/components/pensum/Pensum.jsx
@@ -10,6 +10,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
+    flex: 1,
     padding: 10,
     gap: 20,
     backgroundColor: theme.colors.white,
