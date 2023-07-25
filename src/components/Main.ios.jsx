f302a54c80c70373581bf24fe530f3ac40e8c834
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-07-25 12:02:20 -0300

    [UPDATE] router ios

--- src/components/Main.ios.jsx
+++ src/components/Main.ios.jsx
@@ -1,16 +1,16 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Route, Routes, useLocation } from "react-router-native";
 import NavBar from "./NavBar";
-import Home from "./home/Home";
-import Pensum from "./pensum/Pensum";
 import SubjectsInLevelList from "./pensum/SubjectsInLevelList";
-import ClassesToday from "./classestoday/ClassesToday";
-import Schedule from "./schedule/Schedule";
 import Express from "./express/Express";
 import { StatusBar } from "expo-status-bar";
 import { useDeviceOrientation } from "@react-native-community/hooks";
 import Constants from "expo-constants";
 import Header from "./Header";
+import Home from "../pages/home/Home";
+import PensumPage from "../pages/pensum/PensumPage";
+import ClassesTodayPage from "../pages/classestoday/ClassesTodayPage";
+import SchedulePage from "../pages/schedule/SchedulePage";
 
 const styles = StyleSheet.create({
   container: {
@@ -36,10 +36,10 @@ export default function Main() {
       )}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pensum" element={<Pensum />} />
+        <Route path="/pensum" element={<PensumPage />} />
         <Route path="/pensum/:level" element={<SubjectsInLevelList />} />
-        <Route path="/clases-hoy" element={<ClassesToday />} />
-        <Route path="/horario" element={<Schedule />} />
+        <Route path="/clases-hoy" element={<ClassesTodayPage />} />
+        <Route path="/horario" element={<SchedulePage />} />
         <Route path="/express" element={<Express />} />
         <Route path="*" element={<Text>Not Found</Text>} />
       </Routes>
