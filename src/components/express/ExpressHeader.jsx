acd953c65a576485b0b452f244b54e3e7f5b8e2b
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-07-26 18:38:00 -0300

    [ADD] express header

---
+++ src/components/express/ExpressHeader.jsx
@@ -0,0 +1,39 @@
+import { useDeviceOrientation } from "@react-native-community/hooks";
+import { View, StyleSheet } from "react-native";
+import TextStyle from "../TextStyle";
+import { theme } from "../../theme";
+import ExpressOptions from "./ExpressOptions";
+
+export default function ExpressHeader({ action, showCarreras }) {
+  const orientation = useDeviceOrientation();
+  const styHeader = [
+    styles.header,
+    orientation === "landscape" && {
+      flexDirection: "row-reverse",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+  ];
+  const styTitle = [styles.title];
+  return (
+    <View style={styHeader}>
+      <TextStyle style={styTitle}>
+        Express - Gestión {new Date().getFullYear()}
+      </TextStyle>
+      <ExpressOptions showCarreras={action} showCarrerasValue={showCarreras} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    position: "relative",
+    backgroundColor: theme.colors.primary,
+    padding: 5,
+    gap: 10,
+  },
+  title: {
+    color: theme.colors.white,
+    fontSize: 15,
+  },
+});
