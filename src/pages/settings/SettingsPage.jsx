378f6b0ed2f698177c9935bd3c850d02f53b457b
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-07-28 10:49:25 -0300

    [UPDATE] settings page

--- src/pages/settings/SettingsPage.jsx
+++ src/pages/settings/SettingsPage.jsx
@@ -1,17 +1,23 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import UserCard from "../../components/home/UserCard";
 import { useContext } from "react";
 import { StudentContext } from "../../context/studentContext";
 import RememberClasses from "../../components/settingsApp/RememberClasses";
 import { theme } from "../../theme";
 import TextStyle from "../../components/TextStyle";
+import EditIcon from "../../components/icons/EditIcon";
 
 export default function SettingsPage() {
   const { student } = useContext(StudentContext);
   return (
     <View style={styles.container}>
-      <View style={styles.cuenta}>
-        <TextStyle style={styles.title}>Cuenta</TextStyle>
+      <View style={styles.account}>
+        <View style={styles.accountHeader}>
+          <TextStyle style={styles.title}>Cuenta</TextStyle>
+          <Pressable>
+            <EditIcon width={20} height={20} color={theme.colors.primary} />
+          </Pressable>
+        </View>
         <UserCard name={student.name} />
       </View>
       <View
@@ -32,12 +38,16 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.white,
     gap: 10,
   },
-  cuenta: {
+  account: {
     backgroundColor: theme.colors.white,
   },
   title: {
     fontSize: 20,
     color: theme.colors.primary,
     margin: 10,
+  },
+  accountHeader: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
